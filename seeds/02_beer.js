ba21e993d49f9f63e80af15c85282150ed8186cd
Jason Clifton <j.clifton1473@gmail.com>
2017-08-04 12:12:28 -0600

    Added bollean for on_tap

--- seeds/02_beer.js
+++ seeds/02_beer.js
@@ -13,7 +13,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Very Good",
           "user_id": null,
           "description": "Ruby marmalade color. It is distinctive enough to like your first go at it, but also nicely balanced and drinkable enough to ensure its staying power. Its most notable flavor notes are caramel malt, finishing with a dry, somewhat tongue-coating hoppy finish.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/Tower_ESB_2015.jpg?7412647295741330827"
+          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/Tower_ESB_2015.jpg?7412647295741330827",
+          "on_tap": true
         }, {
           "name": "AllGood Amber",
           "type": "Amber Ale",
@@ -22,7 +23,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Good",
           "user_id": null,
           "description": "Antique amber color. Allgood Amber Ale® becomes more appreciated the more you get acquainted with it. It doesn’t slap you in the face with hops or alcohol on the first sip or two. Rather, its satisfying balance of flavors, along with its ability to be both flavorful and very drinkable, makes this beer universally appealing regardless of mood or situation.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/Allgood_2015.jpg?3008890982502804742"
+          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/Allgood_2015.jpg?3008890982502804742",
+          "on_tap": true
         }, {
           "name": "4.0 GPA",
           "type": "Grapefruit",
@@ -31,7 +33,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Very Good",
           "user_id": null,
           "description": "At the confluence of grapefruit and hops, there exists the perfect summertime ale. Whole grapefruit are each lovingly caressed, peeled and juiced by the hands of our meticulous brewers while they whistle and dream of warm summer nights yet to come. Enjoy the fruits of their labor.  Finally, you too can have a 4.0 GPA.  Your Mom will be proud.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/GPA_2015.jpg?11807025853315381311"
+          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/GPA_2015.jpg?11807025853315381311",
+          "on_tap": false
         }, {
           "name": "Happy Hop Pilsner",
           "type": "Bohemian Pilsner",
@@ -40,7 +43,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Very Good",
           "user_id": null,
           "description": "Brilliant gold color. Soft, dry, malty aroma with a delicate hop presence. Light in body. Soft, clean, delicately malty flavors becoming gently dry on the finish. A lager should be thirst quenching and it should have character too. This beer accomplishes both– and with style!",
-          "image_url": ""
+          "image_url": "",
+          "on_tap": false
         }, {
           "name": "Big Ben Brown",
           "type": "Brown Ale",
@@ -49,7 +53,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Very Good",
           "user_id": null,
           "description": "Deep dark mahogany color. Big Ben Brown Ale® has rich, seductive flavors of molasses, chewy toffee, and dark pit fruit, all leading to a roasted malty finish.  A great beer with a burger or a steak.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/BigBen_2015.jpg?4545272721637645871"
+          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/BigBen_2015.jpg?4545272721637645871",
+          "on_tap": false
         }, {
           "name": "Wit Beer",
           "type": "Belgian-Style Ale",
@@ -58,7 +63,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Very Good",
           "user_id": null,
           "description": "Slightly higher in alcohol content than traditional Wits, ours is brewed using wheat, oats, authentic Belgian yeast, malts and just a sprinkle of hops. Spices include coriander, sweet and bitter orange peel and grains of paradise.",
-          "image_url": "http://www.epicbrewing.com/media/k2/items/cache/97d1d3d17841d6d3353ed857f6573ac4_S.jpg"
+          "image_url": "http://www.epicbrewing.com/media/k2/items/cache/97d1d3d17841d6d3353ed857f6573ac4_S.jpg",
+          "on_tap": false
         }, {
           "name": "Blue Ski",
           "type": "Lager",
@@ -76,7 +82,8 @@ exports.seed = function(knex, Promise) {
           "bar": "Good",
           "user_id": null,
           "description": "Hints of raspberry bubble gum flavors from the Belgian yeast and fresh raspberries tease the pallet in this Brainless® offering.",
-          "image_url": "http://www.epicbrewing.com/media/k2/items/cache/24fae0cf4e190078d5b9896e00870cd9_S.jpg"
+          "image_url": "http://www.epicbrewing.com/media/k2/items/cache/24fae0cf4e190078d5b9896e00870cd9_S.jpg",
+          "on_tap": false
         },
       ]);
     });
