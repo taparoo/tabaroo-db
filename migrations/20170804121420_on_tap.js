2e205c22f2393b8e89ddfde02200eeb8d5bde976
Robert Puls <rpu1s@sbcglobal.net>
2017-08-04 18:06:13 -0600

    put route for taps

--- migrations/20170804121420_on_tap.js
+++ migrations/20170804121420_on_tap.js
@@ -2,14 +2,8 @@ exports.up = function(knex, Promise) {
   return knex.schema
   .createTable("on-tap", table => {
     table.increments("id");
-    table.string("name");
-    table.string("type");
-    table.string("brewery");
-    table.float("abv", [2]);
-    table.string("bar");
-    table.integer("user_id").references("user.id").unsigned().onDelete("cascade");
-    table.text("description");
-    table.text("image_url").defaultTo("http://www.beertastes.com/img/beericon.png");
+    table.integer("beer_id").references("beer.id");
+    table.string("tap");
   });
 };
 
--- seeds/03_on_tap.js
+++ seeds/03_on_tap.js
@@ -6,33 +6,15 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('on-tap').insert([
         {
-          "name": "Tower E.S.B.",
-          "type": "Extra Special Bitter",
-          "brewery": "Bull & Bush",
-          "abv": 6.8,
-          "bar": "Very Good",
-          "user_id": null,
-          "description": "Ruby marmalade color. It is distinctive enough to like your first go at it, but also nicely balanced and drinkable enough to ensure its staying power. Its most notable flavor notes are caramel malt, finishing with a dry, somewhat tongue-coating hoppy finish.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/Tower_ESB_2015.jpg?7412647295741330827"
+          "beer_id": 1,
+          "tap": "left"
         }, {
-          "name": "AllGood Amber",
-          "type": "Amber Ale",
-          "brewery": "Bull & Bush",
-          "abv": 6,
-          "bar": "Good",
-          "user_id": null,
-          "description": "Antique amber color. Allgood Amber Ale® becomes more appreciated the more you get acquainted with it. It doesn’t slap you in the face with hops or alcohol on the first sip or two. Rather, its satisfying balance of flavors, along with its ability to be both flavorful and very drinkable, makes this beer universally appealing regardless of mood or situation.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/Allgood_2015.jpg?3008890982502804742"
+          "beer_id": 2,
+          "tap": "right"
         }, {
-          "name": "4.0 GPA",
-          "type": "Grapefruit",
-          "brewery": "Bull & Bush",
-          "abv": 4,
-          "bar": "Very Good",
-          "user_id": null,
-          "description": "At the confluence of grapefruit and hops, there exists the perfect summertime ale. Whole grapefruit are each lovingly caressed, peeled and juiced by the hands of our meticulous brewers while they whistle and dream of warm summer nights yet to come. Enjoy the fruits of their labor.  Finally, you too can have a 4.0 GPA.  Your Mom will be proud.",
-          "image_url": "https://cdn.shopify.com/s/files/1/0272/0957/files/GPA_2015.jpg?11807025853315381311"
-        }
+          "beer_id": 3,
+          "tap": "cooler"
+        },
       ]);
     });
 };
